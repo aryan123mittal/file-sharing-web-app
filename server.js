2de7771763598180980d06d00e6faec94fdc69da
aryan123mittal <136236033+aryan123mittal@users.noreply.github.com>
2024-10-07 22:03:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port= process.env.PORT || 3000;
+
+ const connectDB = require('./config/db');
+ connectDB();
+ 
+
+app.listen(port, () =>{
+    console.log(`listening on port ${port}`);
+})
